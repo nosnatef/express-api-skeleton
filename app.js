fa0e62995a26b176b0ac4275ce056accc5841507
ianbrown9475 <ian.brown9475@gmail.com>
2019-07-16 11:05:19 -0700

    Add global response validation, use uri in openapi

--- app.js
+++ app.js
@@ -77,6 +77,46 @@ const errorTransformer = (openapiError, ajvError) => {
 };
 
 /**
+ * @summary Middleware that overrides res.send to perform response validation
+ * @function
+ */
+const validateAllResponses = (req, res, next) => {
+  const strictValidation = req.apiDoc['x-express-openapi-validation-strict'];
+  if (typeof res.validateResponse === 'function') {
+    const { send } = res;
+    res.send = (...args) => {
+      const onlyWarn = !strictValidation;
+      if (res.get('x-express-openapi-validation-error-for') !== undefined) {
+        return send.apply(res, args);
+      }
+      const body = args[0];
+      let validation = res.validateResponse(res.statusCode, body);
+      let validationMessage;
+      if (validation === undefined) {
+        validation = { message: undefined, errors: undefined };
+      }
+      if (validation.errors) {
+        let errors;
+        try {
+          errors = JSON.stringify(validation.errors, null, 2);
+        } catch (err) {
+          return errorHandler(res, err);
+        }
+        validationMessage = `Invalid response for status code ${res.statusCode}: ${errors}`;
+        console.warn(validationMessage);
+        // Set to avoid a loop, and to provide the original status code
+        res.set('x-express-openapi-validation-error-for', res.statusCode.toString());
+      }
+      if (onlyWarn || !validation.errors) {
+        return send.apply(res, args);
+      }
+      return errorHandler(res, validationMessage);
+    };
+  }
+  next();
+};
+
+/**
  * @summary Return API meta information at admin endpoint
  */
 adminAppRouter.get(`${openapi.basePath}`, async (req, res) => {
@@ -103,7 +143,11 @@ adminAppRouter.get(`${openapi.basePath}`, async (req, res) => {
  */
 initialize({
   app: appRouter,
-  apiDoc: openapi,
+  apiDoc: {
+    ...openapi,
+    'x-express-openapi-additional-middleware': [validateAllResponses],
+    'x-express-openapi-validation-strict': false,
+  },
   paths: `${appRoot}/api${openapi.basePath}/paths`,
   consumesMiddleware: {
     'application/json': compose([bodyParser.json(), bodyParserError]),
